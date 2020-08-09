bc6572807f6bf85c274a0f4bcdf22d974b2d3677
JoshN22 <j.w.newman@hotmail.com>
2020-08-08 21:40:38 -0230

    Added menu for car customization
    Added menu for car customization, car preview image wont load.

--- src/main/java/MooseGameMenu.java
+++ src/main/java/MooseGameMenu.java
@@ -7,35 +7,47 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.TitledPane;
-import javafx.scene.input.KeyCode;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Box;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-
-import java.awt.*;
 
 import static com.almasb.fxgl.dsl.FXGL.getUIFactoryService;
 import static javafx.scene.paint.Color.*;
 
 public class MooseGameMenu extends FXGLMenu {
 
+    private Node mainMenuScreen;
+    private Node leaderboardScreen;
+    private Node carSelectionScreen;
+
     public MooseGameMenu() {
         super(MenuType.MAIN_MENU);
+        mainMenuScreen = createMainMenu();
+        showMainMenu();
+        carSelectionScreen = carSelectionMenu();
+    }
 
+//    private Node leaderboardMenu(){
+//
+//    }
+
+    private Node createMainMenu(){
         MooseButton playButton = new MooseButton("Play Game", () -> fireNewGame());
         MooseButton leaderboardButton = new MooseButton("Leaderboard", () -> fireNewGame());
+        MooseButton carSelectionButton = new MooseButton("Car Selection", () -> showSelectionMenu());
         MooseButton creditButton = new MooseButton("Credits", () -> fireNewGame());
         MooseButton quitButton = new MooseButton("Exit Game", () -> fireExit());
 
         MooseGameMenuTitle title = new MooseGameMenuTitle("Moose On The Loose");
 
-        var box = new VBox(15, playButton, creditButton, quitButton, leaderboardButton);
+        var box = new VBox(15, playButton, leaderboardButton, carSelectionButton, creditButton, quitButton);
         var titleBox = new VBox (title);
+        var menuBox = new VBox(titleBox, box);
 
         titleBox.setTranslateX(30);
         titleBox.setTranslateY(200);
@@ -45,11 +57,62 @@ public class MooseGameMenu extends FXGLMenu {
         box.setTranslateX(175);
         box.setTranslateY(400);
         box.setBackground(new Background(new BackgroundFill(BLACK, null, null)));
-        box.setPadding(new Insets(50,50,50,50));
+        box.setPadding(new Insets(50));
 
-
-        getContentRoot().getChildren().addAll(box, titleBox);
+        return menuBox;
     }
+
+    private void showMainMenu(){
+//        Image img = new Image("file:Roadway.png");
+//        ImageView bg = new ImageView(img);
+//        getContentRoot().getChildren().add(0, bg);
+        getMenuContentRoot().getChildren().clear();
+        getMenuContentRoot().getChildren().addAll(mainMenuScreen);
+    }
+
+    private void showSelectionMenu(){
+//        Image img = new Image("file:Roadway.png");
+//        ImageView bg = new ImageView(img);
+//        getContentRoot().getChildren().add(0, bg);
+        getMenuContentRoot().getChildren().clear();
+        getMenuContentRoot().getChildren().addAll(carSelectionScreen);
+    }
+
+    private Node carSelectionMenu(){
+        StackPane pane = new StackPane();
+        pane.setPrefSize(600,900);
+        pane.setAlignment(Pos.CENTER);
+
+        VBox box = new VBox();
+        box.setPrefSize(300, 450);
+        Button car1Button = new Button("Car 1");
+        car1Button.setOnAction(((event) -> {SelectCar1();}));
+        box.getChildren().add(car1Button);
+
+        Button car2Button = new Button("Car 2");
+        car2Button.setOnAction(((event) -> {SelectCar2();}));
+        box.getChildren().add(car2Button);
+
+        pane.getChildren().add(box);
+
+        //Image img = new Image("Car_AM_General_Hummer_98x164.png");
+        ImageView carView = new ImageView("file:Roadway.png");
+        Rectangle rect = new Rectangle();
+        rect.setStyle("-fx-background-color: black");
+        rect.setX(300);
+        pane.getChildren().add(rect);
+
+        return pane;
+    }
+
+    private void SelectCar1(){
+
+    }
+
+    private void SelectCar2(){
+
+    }
+
 
     @Override
     protected Button createActionButton(StringBinding stringBinding, Runnable runnable) {
