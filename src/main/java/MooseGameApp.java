3192418ec81e046c8cbeff0fdd5188a0519b7d4b
Michael Power <michaelpower27@hotmail.ca>
2020-08-10 17:25:07 -0230

    Fixed loading leaderboards into leaderboard menu.

--- src/main/java/MooseGameApp.java
+++ src/main/java/MooseGameApp.java
@@ -4,7 +4,6 @@ import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.app.scene.FXGLMenu;
 import com.almasb.fxgl.app.scene.SceneFactory;
 import com.almasb.fxgl.audio.Music;
-import com.almasb.fxgl.core.collection.Array;
 import com.almasb.fxgl.core.math.FXGLMath;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
@@ -19,7 +18,8 @@ import java.util.Map;
 import static com.almasb.fxgl.dsl.FXGL.*;
 
 /**
- * Main game loop app for Moose on the Loose.
+ * Main game loop app for Moose on the Loose. This class is responsible for setting the game's settings,
+ * its ui, its controls, its entities, its physics, as well as saving and loading highscore data.
  */
 public class MooseGameApp extends GameApplication {
     private Entity player;
@@ -27,7 +27,7 @@ public class MooseGameApp extends GameApplication {
     private Entity background2;
     private Entity signpost;
     private Music bgm;
-    private SaveData savedData = null;
+    private SaveData saveData = null;
     private int highScore;
     private String highScoreName;
     private ArrayList<HighScore> highScoreList;
@@ -52,6 +52,10 @@ public class MooseGameApp extends GameApplication {
         });
     }
 
+    /**
+     * Sets game variables.
+     * @param vars vars
+     */
     @Override
     protected void initGameVars(Map<String, Object> vars) {
         vars.put("score", 0);
@@ -59,12 +63,19 @@ public class MooseGameApp extends GameApplication {
         vars.put("speed",150);
     }
 
+    /**
+     * Loads a FXML file and sets it as the overlay for UI.
+     */
     @Override
     protected void initUI(){
         UI ui = getAssetLoader().loadUI("moosegame_ui.fxml", new MooseGameUIController());
         getGameScene().addUI(ui);
     }
 
+    /**
+     * Initializes the controls. Currently only used for the player, but could be used for any entity.
+     * Sets the WASD keys as well as the Arrow keys to control the player's movement. Also allows for changing the car's speed with 1, 2 and 3.
+     */
     @Override
     protected void initInput() {
         onKey(KeyCode.A, () -> player.getComponent(PlayerComponent.class).moveLeft());
@@ -82,6 +93,9 @@ public class MooseGameApp extends GameApplication {
         onKey(KeyCode.UP, () -> player.getComponent(PlayerComponent.class).moveUp());
     }
 
+    /**
+     * Starting a game. This method is overloaded and calls another initGame after it loads the save data. If no save data is found, create it.
+     */
     @Override
     protected void initGame(){
 
@@ -89,20 +103,23 @@ public class MooseGameApp extends GameApplication {
 
 
         getFileSystemService().<SaveData>readDataTask("./highscores.dat")
-                .onSuccess(data -> savedData = data)
+                .onSuccess(data -> saveData = data)
                 .onFailure(error->{error.printStackTrace();})
                 .run();
-        if (savedData == null){
-            savedData = new SaveData(new ArrayList<HighScore>());
-            savedData.getHighScoreList().add(new HighScore("CPU",0));
-            savedData.getHighScoreList().add(new HighScore("CPU",0));
-            savedData.getHighScoreList().add(new HighScore("CPU",0));
+        if (saveData == null){
+            saveData = new SaveData(new ArrayList<HighScore>());
+            saveData.getHighScoreList().add(new HighScore("Eric  ",100));
+            saveData.getHighScoreList().add(new HighScore("Josh",50));
+            saveData.getHighScoreList().add(new HighScore("Pranjal",0));
             System.out.println("new scorelist created.");
         }
-        initGame(savedData);
+        initGame(saveData);
     }
 
-
+    /**
+     * The main initGame method, this time with save data. Sets the background, grabs the highscores (to display later), spawns the players, the signpost, and all other entities.
+     * @param saveData
+     */
     protected void initGame(SaveData saveData){
         bgm = getAssetLoader().loadMusic("Poisoncut_GameLoop.mp3");
         getAudioPlayer().loopMusic(bgm);
@@ -122,6 +139,9 @@ public class MooseGameApp extends GameApplication {
         run(()->FXGL.getWorldProperties().setValue("time",FXGL.getWorldProperties().getInt("time")-1),Duration.seconds(2));
     }
 
+    /**
+     * Physics handler, this method is responsible for all the collisions in the game.
+     */
     protected void initPhysics(){
 
         onCollisionBegin(EntityType.PLAYER,EntityType.GASTANK, (player, gastank) -> {
@@ -170,6 +190,10 @@ public class MooseGameApp extends GameApplication {
         });
     }
 
+    /**
+     * Handles the movement of all entities besides the player. All movement is tied to the "speed" world property that was set in initVars.
+     * @param tpf time per frame
+     */
     @Override
     protected void onUpdate(double tpf){
         getGameWorld().getEntitiesByType(EntityType.GASTANK).forEach(gastank -> gastank.translateY(FXGL.getWorldProperties().getInt("speed")*tpf));
@@ -201,9 +225,12 @@ public class MooseGameApp extends GameApplication {
         }
     }
 
+    /**
+     * Game over handler. Spawns an explosion at the player, stops the music, then displays a random moose fact.
+     */
     private void gameOver() {
 
-
+        int score = FXGL.getWorldProperties().getInt("score");
         Point2D explosionSpawnPoint = player.getPosition().subtract(30, 0);
         spawn("explosion", explosionSpawnPoint);
 
@@ -220,12 +247,33 @@ public class MooseGameApp extends GameApplication {
         getDialogService().showConfirmationBox("Did you know?\n"+randomMessage+"\nPlay Again?", yes -> {
             if (yes) {
                 getGameWorld().getEntitiesCopy().forEach(Entity::removeFromWorld);
-                getGameController().startNewGame();
-            } else {
-
-                int score = FXGL.getWorldProperties().getInt("score");
                 if (score > highScoreList.get(highScoreList.size()-1).getScore()) {
-                    getDialogService().showInputBox("High Score! Enter your name", playerName -> {
+                    getDialogService().showInputBox("High Score! Enter your name\n"+
+                                    "1.\t" + highScoreList.get(0).getName() +"\t\t\t" + Integer.toString(highScoreList.get(0).getScore())+"\n"+
+                                    "2.\t" + highScoreList.get(1).getName() +"\t\t\t" + Integer.toString(highScoreList.get(1).getScore())+"\n"+
+                                    "3.\t" + highScoreList.get(2).getName() +"\t\t\t" + Integer.toString(highScoreList.get(2).getScore()),
+                            playerName -> {
+
+                                for (int i = 0; i < highScoreList.size(); i++) {
+                                    if (score > highScoreList.get(i).getScore()) {
+                                        highScoreList.add(i, new HighScore(playerName, score));
+                                        highScoreList.sort(new sortHighScores());
+                                        highScoreList.remove(highScoreList.size() - 1);
+                                        break;
+                                    }
+                                }
+                                getFileSystemService().writeDataTask(new SaveData(highScoreList), "./highscores.dat").run();
+                                getGameController().startNewGame();
+                            });
+                }
+            } else {
+                if (score > highScoreList.get(highScoreList.size()-1).getScore()) {
+                    getDialogService().showInputBox("High Score! Enter your name\n"+
+                            "1.\t" + highScoreList.get(0).getName() +"\t\t\t" + Integer.toString(highScoreList.get(0).getScore())+"\n"+
+                            "2.\t" + highScoreList.get(1).getName() +"\t\t\t" + Integer.toString(highScoreList.get(1).getScore())+"\n"+
+                            "3.\t" + highScoreList.get(2).getName() +"\t\t\t" + Integer.toString(highScoreList.get(2).getScore()),
+                            playerName -> {
+
                         for (int i = 0; i < highScoreList.size(); i++) {
                             if (score > highScoreList.get(i).getScore()){
                                 highScoreList.add(i,new HighScore(playerName,score));
@@ -233,13 +281,15 @@ public class MooseGameApp extends GameApplication {
                                 highScoreList.remove(highScoreList.size()-1);
                                 break;
                             }
-                            System.out.println(highScoreList.get(i).getName());
                         }
                         getFileSystemService().writeDataTask(new SaveData(highScoreList), "./highscores.dat").run();
                         getGameController().gotoMainMenu();
                     });
                 } else {
-                    getGameController().gotoMainMenu();
+                    getDialogService().showMessageBox("High Scores:\n"+
+                            "1.\t" + highScoreList.get(0).getName() +"\t\t\t" + Integer.toString(highScoreList.get(0).getScore())+"\n"+
+                            "2.\t" + highScoreList.get(1).getName() +"\t\t\t" + Integer.toString(highScoreList.get(1).getScore())+"\n"+
+                            "3.\t" + highScoreList.get(2).getName() +"\t\t\t" + Integer.toString(highScoreList.get(2).getScore()),getGameController()::gotoMainMenu);
                 }
             }
         });
